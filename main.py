24df1b7c81f5096b52a69db3304a1a1d484ff072
sasf227 <131782371+sasf227@users.noreply.github.com>
2026-02-02 10:12:53 +0100

    a

--- main.py
+++ main.py
@@ -71,6 +71,30 @@ def logout():
     
     
     
+    
+@app.route('/create', methods=['GET', 'POST'])
+def create():
+    if request.method == 'GET':
+        return render_template('create.html')
+    text=request.form['text']
+    new_item = Textify(text=text)
+    db.session.add(new_item)
+    db.session.commit()
+    return redirect(url_for('home'))
+
+@app.route('/update/')
+def update():
+    return render_template('update.html')
+
+
+@app.route('/delete/<int:id>', methods=['POST'])
+def delete(id):
+    item = Textify.query.get_or_404(id)
+    db.session.delete(item)
+    db.session.commit()
+    return redirect(url_for('home'))
+    
+    
 if __name__ == "__main__":
     with app.app_context():
         db.create_all()
